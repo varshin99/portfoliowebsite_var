2f3bca67dcc2450e6f9ad2032ebaec875f7886d8
Varshin_bhaskaran <59829943+varshin99@users.noreply.github.com>
2024-02-18 16:25:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ document.querySelectorAll('nav ul li a').forEach(link => {
         this.classList.add('active');
     });
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tab");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.tabTarget);
+            
+            tabContents.forEach(tc => {
+                tc.classList.remove("active");
+            });
+            
+            tabs.forEach(t => {
+                t.classList.remove("active");
+            });
+            
+            tab.classList.add("active");
+            target.classList.add("active");
+        });
+    });
+});
