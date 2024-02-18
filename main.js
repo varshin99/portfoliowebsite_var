0ccc8fbc3548690efc0fc3f912ab6a206bb652b9
Varshin_bhaskaran <59829943+varshin99@users.noreply.github.com>
2024-02-18 10:12:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,66 @@
-// SHOW MENU
+const navLinks = document.getElementById("nav-links");
+const menuBtn = document.getElementById("menu-btn");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-const showMenu = (toggleId, navId) => {
-      const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId)
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
 
-      if(toggle && nav){
-            toggle.addEventListener('click', () =>{
-                  nav.classList.toggle('show')
-            });
-      }
-}
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
 
-showMenu('nav_toggle','nav_menu')
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
 
-// ACTIVE & REMOVE ACTIVE
-const navLink = document.querySelectorAll('.nav_link')
-navLink.forEach(n => n.classList.remove('active'))
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
 
-function linkAction(){
-      navLink.forEach(n => n.classList.remove('active'))
-      this.classList.add('active')
+// header container
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+});
 
-      const navMenu = document.getElementById('nav_menu')
-      navMenu.classList.remove('show')
-}
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+ScrollReveal().reveal(".header__content .header__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// about container
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .about__btn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+// service container
+ScrollReveal().reveal(".service__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+// portfolio container
+ScrollReveal().reveal(".portfolio__card", {
+  duration: 1000,
+  interval: 500,
+});
